701a14d03d2746feed75c873e07f46f8e772b211
Nicolas <nicolasgabriel.developer@gmail.com>
2023-01-21 10:28:18 -0300

    Estilizando o texto

--- src/components/Texto.vue
+++ src/components/Texto.vue
@@ -1,5 +1,6 @@
 <template>
-    <h2>oi</h2>
+    <h2>How did we do?</h2>
+    <p>Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</p>
 </template>
 
 <script lang="ts">
