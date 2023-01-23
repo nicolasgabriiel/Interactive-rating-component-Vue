8e2c28b25abc245667858be7ad78c33e491e9309
Nicolas <nicolasgabriel.developer@gmail.com>
2023-01-23 20:16:04 -0300

    Estruturando a mensagem

--- src/components/Mensagem.vue
+++ src/components/Mensagem.vue
@@ -1,5 +1,8 @@
 <template>
-    <h2>ola</h2>
+    <section>
+    <h2>Thank you!</h2>
+    <p>We appreciate you taking the time to give a rating. If you ever need more support, don't hesitate to get in touch!</p>
+    </section>
 </template>
 
 <script lang="ts">
